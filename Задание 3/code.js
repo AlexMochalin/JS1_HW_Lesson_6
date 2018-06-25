846ecbf247e3bc577b0334de5757e883df4eb682
AlexMochalin <alemochalin@yandex.ru>
2018-06-26 02:13:21 +0300

    Внесены изменения

--- Задание 3/code.js
+++ Задание 3/code.js
@@ -9,12 +9,12 @@
 function init(){
 var nextImg = document.getElementById('nextImg');
 nextImg.onclick = function(){
-	//alert('svsdf');
 	var image = document.getElementById('img');
 	var newImage = document.createElement('img');
 	var src = 'img/bigGallery/' + i + '.jpg'; 
 	//newImage.innerHTML = imagesArray[i];
-	newImage.src = null;
+	newImage.src = ' ';
+	image.appendChild(newImage);
 	newImage.src = src;
 	image.appendChild(newImage);
 	i++;
